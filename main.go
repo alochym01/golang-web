9019fab4561acb44d5789d38a03ea66d0adafca9
Your Name <you@example.com>
2020-03-27 15:36:22 +0700

    sample http server in go

--- main.go
+++ main.go
@@ -2,64 +2,33 @@ package main
 
 import (
 	"fmt"
-	"math"
+	"log"
+	"net/http"
 )
 
-// Rectangle shape
-type Rectangle struct {
-	Height float64
-	Width  float64
-}
-
-// Circle shape
-type Circle struct {
-	Radius float64
-}
-
-// Shape is an interface with area function
-type Shape interface {
-	area() float64
-}
-
-func (r Rectangle) area() float64 {
-	return r.Height * r.Width
-}
-
-func (c Circle) area() float64 {
-	return c.Radius * c.Radius * math.Pi
+func sayHello(res http.ResponseWriter, req *http.Request) {
+	fmt.Println(req.URL)
+	fmt.Println(req.URL.Scheme)
+	fmt.Fprintf(res, "Hello\n")
 }
 
 func main() {
-	// declare s1/s2 as Shape inteface
-	var s1 Shape
-	var s2 Shape
 
-	// init r as Rectangle shape
-	r := Rectangle{
-		12,
-		2,
+	// using sayHello to handle request
+	http.HandleFunc("/", sayHello)
+
+	// using embeded function to handle request
+	http.HandleFunc("/alochym", func(res http.ResponseWriter, req *http.Request) {
+		fmt.Println(req.URL)
+		fmt.Println(req.URL.Scheme)
+		fmt.Fprintf(res, "Hello Alochym\n")
+	})
+
+	// starting http server w port 500
+	err := http.ListenAndServe(":5000", nil)
+
+	if err != nil {
+		log.Fatal("listen ans serve: ", err)
 	}
-	// using interface
-	s1 = r
-
-	// init c as Circle shape
-	c := Circle{
-		100,
-	}
-	// using interface
-	s2 = c
-
-	// using interface
-	fmt.Println(s1.area())
-	fmt.Println(s2.area())
-
-	fmt.Println("===============")
-
-	// using method
-	fmt.Println(r.area())
-	fmt.Println(c.area())
-
-	fmt.Println("===============")
-
-	fmt.Println("main function")
+	fmt.Println("server running")
 }
